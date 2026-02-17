f49b416e240df09a06b0da0790072a836699d2ed
Singh <akrsingh@ptc.com>
2026-02-17 22:43:54 +0530

    Fixed CI driver handling

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -1,36 +1,44 @@
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 from utils.config_reader import ConfigReader
 from utils.logger import Logger
-import os
 
 class DriverFactory:
 
     def get_driver(self):
         config = ConfigReader()
         browser = config.get_browser()
-        headless = config.is_headless()
-
         logger = Logger.get_logger()
+
         logger.info("Initializing WebDriver")
+
+        options = Options()
+
+        # ✅ Headless for CI
+        if os.getenv("CI") == "true":
+            options.add_argument("--headless=new")
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-dev-shm-usage")
+            logger.info("Running in CI → Headless mode enabled")
 
         if browser.lower() == "chrome":
 
-            driver_path = os.path.join("drivers", "chromedriver.exe")
-            logger.info(f"Using local driver: {driver_path}")
+            # ✅ Use webdriver-manager in CI
+            if os.getenv("CI") == "true":
+                logger.info("Using webdriver-manager for CI")
+                service = Service(ChromeDriverManager().install())
 
-            options = Options()
+            else:
+                driver_path = os.path.join("drivers", "chromedriver.exe")
+                logger.info(f"Using local driver: {driver_path}")
+                service = Service(driver_path)
 
-            if headless:
-                logger.info("Running in HEADLESS mode")
-                options.add_argument("--headless=new")
-
-            options.add_argument("--start-maximized")
-
-            service = Service(driver_path)
             driver = webdriver.Chrome(service=service, options=options)
 
+            driver.maximize_window()
             driver.implicitly_wait(config.get_implicit_wait())
             driver.get(config.get_base_url())
 
