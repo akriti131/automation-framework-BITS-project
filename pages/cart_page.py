0ce73838b68e9bd508eb01b827bafaf86df8e5d4
Singh <akrsingh@ptc.com>
2026-05-16 12:41:30 +0530

    Added explicit waits to cart page

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -1,18 +1,30 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class CartPage:
 
     def __init__(self, driver):
         self.driver = driver
+        self.wait = WebDriverWait(driver, 20)
 
     # Locators
+    product_name = (By.CLASS_NAME, "inventory_item_name")
+
     checkout_btn = (By.ID, "checkout")
-    inventory_item = (By.CLASS_NAME, "inventory_item_name")
 
     # Actions
     def get_product_name(self):
-        return self.driver.find_element(*self.inventory_item).text
+
+        element = self.wait.until(
+            EC.visibility_of_element_located(self.product_name)
+        )
+
+        return element.text
 
     def click_checkout(self):
-        self.driver.find_element(*self.checkout_btn).click()
+
+        self.wait.until(
+            EC.element_to_be_clickable(self.checkout_btn)
+        ).click()
